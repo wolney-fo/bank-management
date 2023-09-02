25a78b9e4c4063012a41db7200445a756526c56c
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2023-09-01 22:25:51 -0300

    feat: transfer

--- BankManagement/main.c
+++ BankManagement/main.c
@@ -21,13 +21,14 @@ int main()
 
     const char LOCAL_CURRENCY[4] = "GBR";
     int option;
-    double withdrawal, deposit;
+    double withdrawal, deposit, transfer;
 
     do {
         printf("Hello, Mr. %s\n", currentUser.name);
         printf("[ 1 ] Balance\n");
         printf("[ 2 ] Withdrawal\n");
         printf("[ 3 ] Deposit\n");
+        printf("[ 4 ] Transfer\n");
         printf("[ 0 ] Exit\n\n");
         printf(">");
         scanf_s("%d", &option);
@@ -62,7 +63,7 @@ int main()
 
             break;
         case 3:
-            printf("\n\Deposit\n");
+            printf("\n\nDeposit\n");
             printf("Value: %s ", LOCAL_CURRENCY);
             scanf_s("%lf", &deposit);
             getchar();
@@ -81,6 +82,33 @@ int main()
             }
 
             printf("Successful deposit!\n\n");
+            break;
+        case 4:
+            puts("Transfer money");
+            printf("Value %s", LOCAL_CURRENCY);
+            scanf_s("%lf", &transfer);
+            getchar();
+
+            // Verify if transfer is possible
+            if (transfer <= currentAccount.balance + currentAccount.overdraft) {
+                int reciver_account;
+                printf("Account of reciver: ");
+                scanf_s("%d", &reciver_account);
+
+                // Uses overdraft
+                if (transfer <= currentAccount.balance) {
+                    currentAccount.balance -= transfer;
+                }
+                else {
+                    currentAccount.overdraft -= (transfer - currentAccount.balance);
+                    currentAccount.balance = 0.0;
+                }
+                printf("Successful transfer!\n\n");
+            }
+            else {
+                printf("Unauthorized transfer. Amount exceeds your balance.\n\n");
+            }
+
             break;
         case 0:
             printf("Have a great week!");
